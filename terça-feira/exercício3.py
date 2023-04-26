6cd867aad7a3b2f1e51d19f956bf8b1d5235e045
Pedro Ernesto <pedroernestovogado@gmail.com>
2023-04-25 21:40:59 -0300

    commit terça

---
+++ terça-feira/exercício3.py
@@ -0,0 +1,9 @@
+
+valor_num = float(input("Digite um valor: "))
+
+if valor_num > 0:
+    print("O valor digitado é positivo!")
+elif valor_num < 0:
+    print("O valor digitado é negativo!")
+else:
+    print("O valor digitado é zero!")
