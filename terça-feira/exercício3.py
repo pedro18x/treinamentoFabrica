a4c84725c409df5b4044ede3c2168a04939660f8
Pedro Ernesto <129099420+pedro18x@users.noreply.github.com>
2023-04-25 21:42:42 -0300

    Delete terça-feira directory

--- terça-feira/exercício3.py
+++
@@ -1,9 +0,0 @@
-
-valor_num = float(input("Digite um valor: "))
-
-if valor_num > 0:
-    print("O valor digitado é positivo!")
-elif valor_num < 0:
-    print("O valor digitado é negativo!")
-else:
-    print("O valor digitado é zero!")
