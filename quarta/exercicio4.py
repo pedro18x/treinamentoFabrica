c57cb815f06c9d5c6b0ca8a18c25fa020279c110
Pedro Ernesto <129099420+pedro18x@users.noreply.github.com>
2023-05-10 17:19:03 -0300

    Delete quarta directory

--- quarta/exercicio4.py
+++
@@ -1,7 +0,0 @@
-
-num = float(input("Digite um número real: "))
-
-if num % 2 == 0:
-    print("O número digitado é par.")
-else:
-    print("O número digitado é ímpar.")
--- quarta/exercicio5.py
+++
@@ -1,8 +0,0 @@
-
-salario_recebido = float(input("Informe o seu salário: "))
-gasto_total = float(input("Informe o total gasto em despesas: "))
-
-if gasto_total <= salario_recebido:
-    print("Gastos dentro do orçamento")
-else:
-    print("Gastos acima do orçamento")
