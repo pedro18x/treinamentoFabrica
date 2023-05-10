92f77688ed9c68f9560af739710dfda939d773bc
Pedro Ernesto <129099420+pedro18x@users.noreply.github.com>
2023-05-10 17:19:17 -0300

    Delete sexta directory

--- sexta/exercicio7.py
+++
@@ -1,9 +0,0 @@
-frase = input("Digite uma frase: ")
-
-frase_sem_espacos = frase.replace(" ", "")
-
-num_caracteres = len(frase_sem_espacos)
-
-# exibe a frase e a quantidade de caracteres
-print("A frase digitada foi:", frase)
-print("A quantidade de caracteres (sem espaços) é:", num_caracteres)
